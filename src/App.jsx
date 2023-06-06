dafe9a7e1b4c56b42a31d3decd68b2bb17bdc56a
containerFluid <d.ardiansyah999@gmail.com>
2023-06-06 11:25:15 +0700

    install dependency (chakra UI, react router, framer motion)

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Box, Button, Heading, Center } from "@chakra-ui/react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Center
+        w="100vw"
+        h="100vh"
+        bgGradient="linear(to-b, white, white, gray) animation"
+      >
+        <Heading
+          as="h1"
+          fontSize="4rem"
+          fontFamily="helvetica"
+          color="#242424"
+          transition="all ease-in-out .2s"
+        >
+          Coming Soon
+        </Heading>
+      </Center>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
